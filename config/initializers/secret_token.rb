588722cc449a4cefbb5d4b50280453994f9d936b
Aron Hegyi <ahegyi@gmail.com>
2013-04-10 17:54:02 -0700

    Initial commit with scaffolded products and reviews

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsStore::Application.config.secret_token = 'c1659bd901363c275f64dc94eed8c7e23697acaa1f5d2b35f8512aedeb42a20e0ce3bbf03e5835bea29216f275487bb7692691610228f16a3d6686d5b536a03c'
