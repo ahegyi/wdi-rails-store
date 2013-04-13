5c246dea9a716181dbf950f514bc7f486203d1dc
Aron Hegyi <ahegyi@gmail.com>
2013-04-12 18:32:54 -0700

    Add some validations for products and reviews

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,9 @@ class Product < ActiveRecord::Base
 
   has_many :reviews
 
+  validates_uniqueness_of :name
+  validates :name, :price, :presence => true
+
   def average_rating
     (self.reviews.map {|review| review.rating }.reduce(:+)) / self.reviews.count.to_f
   end
