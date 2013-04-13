19ad8e63d7dce619e783fa3028030bf23cd0b23c
Aron Hegyi <ahegyi@gmail.com>
2013-04-12 18:19:37 -0700

    Cleaned up design of product page, made reviews 'star' style and provided average rating method

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,9 @@ class Product < ActiveRecord::Base
   attr_accessible :name, :price, :body, :image_url, :in_cart
 
   has_many :reviews
+
+  def average_rating
+    (self.reviews.map {|review| review.rating }.reduce(:+)) / self.reviews.count.to_f
+  end
+
 end
